55a435dd781c0ad449c5035ff502dc80c1449bc9
amaumene <alexandre@maumene.org>
2026-03-25 21:46:47 +0900

    fix: remove unused WeeklyStats struct

--- models/resort.go
+++ models/resort.go
@@ -3,18 +3,18 @@ package models
 import "time"
 
 type Resort struct {
-	ID              string    `json:"id" db:"id"`
-	Slug            string    `json:"slug" db:"slug"`
-	Name            string    `json:"name" db:"name"`
-	Prefecture      string    `json:"prefecture" db:"prefecture"`
-	Region          string    `json:"region" db:"region"`
-	TopElevationM   *int      `json:"top_elevation_m" db:"top_elevation_m"`
-	BaseElevationM  *int      `json:"base_elevation_m" db:"base_elevation_m"`
-	VerticalM       *int      `json:"vertical_m" db:"vertical_m"`
-	NumCourses      *int      `json:"num_courses" db:"num_courses"`
-	LongestCourseKM *float64  `json:"longest_course_km" db:"longest_course_km"`
-	SteepestCourseDeg *float64 `json:"steepest_course_deg" db:"steepest_course_deg"`
-	LastUpdated     time.Time `json:"last_updated" db:"last_updated"`
+	ID                string    `json:"id" db:"id"`
+	Slug              string    `json:"slug" db:"slug"`
+	Name              string    `json:"name" db:"name"`
+	Prefecture        string    `json:"prefecture" db:"prefecture"`
+	Region            string    `json:"region" db:"region"`
+	TopElevationM     *int      `json:"top_elevation_m" db:"top_elevation_m"`
+	BaseElevationM    *int      `json:"base_elevation_m" db:"base_elevation_m"`
+	VerticalM         *int      `json:"vertical_m" db:"vertical_m"`
+	NumCourses        *int      `json:"num_courses" db:"num_courses"`
+	LongestCourseKM   *float64  `json:"longest_course_km" db:"longest_course_km"`
+	SteepestCourseDeg *float64  `json:"steepest_course_deg" db:"steepest_course_deg"`
+	LastUpdated       time.Time `json:"last_updated" db:"last_updated"`
 }
 
 type SnowDepthReading struct {
@@ -31,22 +31,13 @@ type DailySnowfall struct {
 	Season     string    `json:"season" db:"season"`
 }
 
-type WeeklyStats struct {
-	ResortID       string    `json:"resort_id" db:"resort_id"`
-	WeekStart      time.Time `json:"week_start" db:"week_start"`
-	WeekEnd        time.Time `json:"week_end" db:"week_end"`
-	TotalSnowfallCM int      `json:"total_snowfall_cm" db:"total_snowfall_cm"`
-	AvgDepthCM     float64   `json:"avg_depth_cm" db:"avg_depth_cm"`
-	NumSnowDays    int       `json:"num_snow_days" db:"num_snow_days"`
-}
-
 type ResortURL struct {
-	BaseURL  string
-	InfoURL  string
-	SnowURL  string
-	Slug     string
+	BaseURL    string
+	InfoURL    string
+	SnowURL    string
+	Slug       string
 	Prefecture string
-	Area     string
+	Area       string
 }
 
 type WeeklyResortStats struct {
@@ -63,18 +54,18 @@ type WeeklyResortStats struct {
 }
 
 type PeakPeriod struct {
-	ID                   string    `json:"id" db:"id"`
-	ResortID             string    `json:"resort_id" db:"resort_id"`
-	PeakRank             int       `json:"peak_rank" db:"peak_rank"`
-	StartDate            string    `json:"start_date" db:"start_date"`
-	EndDate              string    `json:"end_date" db:"end_date"`
-	CenterDate           string    `json:"center_date" db:"center_date"`
-	AvgDailySnowfall     float64   `json:"avg_daily_snowfall" db:"avg_daily_snowfall"`
-	TotalPeriodSnowfall  float64   `json:"total_period_snowfall" db:"total_period_snowfall"`
-	ProminenceScore      float64   `json:"prominence_score" db:"prominence_score"`
-	YearsOfData          int       `json:"years_of_data" db:"years_of_data"`
-	ConfidenceLevel      string    `json:"confidence_level" db:"confidence_level"`
-	CalculatedAt         time.Time `json:"calculated_at" db:"calculated_at"`
+	ID                  string    `json:"id" db:"id"`
+	ResortID            string    `json:"resort_id" db:"resort_id"`
+	PeakRank            int       `json:"peak_rank" db:"peak_rank"`
+	StartDate           string    `json:"start_date" db:"start_date"`
+	EndDate             string    `json:"end_date" db:"end_date"`
+	CenterDate          string    `json:"center_date" db:"center_date"`
+	AvgDailySnowfall    float64   `json:"avg_daily_snowfall" db:"avg_daily_snowfall"`
+	TotalPeriodSnowfall float64   `json:"total_period_snowfall" db:"total_period_snowfall"`
+	ProminenceScore     float64   `json:"prominence_score" db:"prominence_score"`
+	YearsOfData         int       `json:"years_of_data" db:"years_of_data"`
+	ConfidenceLevel     string    `json:"confidence_level" db:"confidence_level"`
+	CalculatedAt        time.Time `json:"calculated_at" db:"calculated_at"`
 }
 
 type ResortWithPeaks struct {
